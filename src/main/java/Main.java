962e382be91083c94d783f2f6d90dd9bb975fbde
F-S-Uyar <142197603+F-S-Uyar@users.noreply.github.com>
2024-08-26 22:30:53 +0300

    inheritance goals completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
 import com.workintech.cylinder.Circle;
 import com.workintech.cylinder.Cylinder;
+import com.workintech.developers.HRManager;
+import com.workintech.developers.JuniorDeveloper;
+import com.workintech.developers.MidDeveloper;
+import com.workintech.developers.SeniorDeveloper;
 import com.workintech.pool.Cuboid;
 import com.workintech.pool.Rectangle;
 
 public class Main {
+
+    public static final String MSG_PREFIX = "sonuç ";
+
     public static void main(String[] args) {
         /*Circle circle = new Circle(3.75);
 
@@ -40,5 +47,33 @@ public class Main {
         System.out.println("cuboid.height= " + cuboid.getHeight());
 
         System.out.println("cuboid.volume= " + cuboid.getVolume());*/
+
+        JuniorDeveloper juniorDeveloper1 = new JuniorDeveloper(34, "mehmet", 5000);
+        JuniorDeveloper juniorDeveloper2 = new JuniorDeveloper(35, "isa", 6000);
+        printResult(MSG_PREFIX, juniorDeveloper1);
+        printResult(MSG_PREFIX, juniorDeveloper2);
+        juniorDeveloper1.work();
+        juniorDeveloper2.work();
+
+        MidDeveloper midDeveloper = new MidDeveloper(1, "ali", 10000);
+        printResult(MSG_PREFIX, midDeveloper);
+        midDeveloper.work();
+
+        HRManager hrManager = new HRManager(5,"kerem", 8000,
+                new JuniorDeveloper[5], new MidDeveloper[6], new SeniorDeveloper[7]);
+        hrManager.addEmployee(0, juniorDeveloper1);
+        hrManager.addEmployee(1, juniorDeveloper2);
+        hrManager.addEmployee(0, midDeveloper);
+
+        System.out.println(hrManager.toString());
+    }
+    private static void printResult(String x, JuniorDeveloper juniorDeveloper) {
+        System.out.println(x + juniorDeveloper.toString());
+    }
+    private static void printResult(String x, MidDeveloper midDeveloper) {
+        System.out.println(x + midDeveloper.toString());
+    }
+    private static void printResult(String x, SeniorDeveloper seniorDeveloper) {
+        System.out.println(x + seniorDeveloper.toString());
     }
 }
